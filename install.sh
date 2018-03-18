9813b23e3faab2b4a9dfbf604dfef21f18cefcfb
Ethical-H4CK3R <Ethical.Hacker720@gmail.com>
2018-03-18 20:36:08 +0000

    archive-key fix

--- install.sh
+++ install.sh
@@ -3,6 +3,9 @@
 # configure the repository file
 echo "deb http://http.kali.org/kali kali-rolling main contrib non-free" > /etc/apt/sources.list
 
+# update outdated archive-key
+wget -q -O - https://archive.kali.org/archive-key.asc | apt-key add
+
 # install Tor && requirements
 apt-get update && apt-get install tor lighttpd php7.0-cgi -y && apt autoremove -y
 pip install -UI -r requirements.txt
