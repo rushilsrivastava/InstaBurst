5e8c7a72791c1fd0be3ea4cd196ef47baea51fa5
Ethical-H4CK3R <Ethical.Hacker70@gmail.com>
2018-03-31 19:11:45 +0000

    subprocess32 fix

--- install.sh
+++ install.sh
@@ -6,12 +6,17 @@ echo "deb http://http.kali.org/kali kali-rolling main contrib non-free" > /etc/a
 # update outdated archive-key
 wget -q -O - https://archive.kali.org/archive-key.asc | apt-key add
 
-# install pip
-apt-get install python-pip python-dev -y
-pip install --upgrade setuptools --user python
+install() {
+ # install pip
+ apt-get install python-pip python-dev -y
+ pip install --upgrade setuptools --user python
 
-# install Tor && requirements
-apt-get update && apt-get install tor lighttpd php7.0-cgi -y && apt autoremove -y
-pip install -UI -r requirements.txt
+ # install Tor && requirements
+ apt-get update && apt-get install tor lighttpd php7.0-cgi -y && apt autoremove -y
+ pip install -UI -r requirements.txt
+}
+
+install
 chmod +x ngrok instagram.py
 chmod -R 775 web
+install
